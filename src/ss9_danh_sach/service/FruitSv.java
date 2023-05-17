036368678da9414beacce1fa31199603c9162d27
quy116 <nguyenphucquy1106@gmail.com>
2023-05-17 14:01:46 +0700

    hello

--- src/ss9_danh_sach/service/FruitSv.java
+++ src/ss9_danh_sach/service/FruitSv.java
@@ -2,28 +2,41 @@ package ss9_danh_sach.service;
 
 import ss9_danh_sach.model.Fruit;
 import ss9_danh_sach.repository.FruitRepository;
+import ss9_danh_sach.validate.Regex;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
-public class FruitSv implements IFruitSv{
+public class FruitSv implements IFruitSv {
     Scanner scanner = new Scanner(System.in);
     FruitRepository fruitRepository = new FruitRepository();
+
     @Override
     public void displaySv() {
         fruitRepository.displayRepo();
     }
 
     @Override
-    public void addSv() {
+    public void addSv()  {
         System.out.println("nhap ten trai cay vao");
-        String inputName = scanner.nextLine();
+        String inputName;
+        do {
+            System.out.println("nhap ten trai cay vao");
+            inputName = scanner.nextLine();
+        } while (!Regex.regexName(inputName));
 
         System.out.println("nhap loai trai cay vao");
         String inputKindOfFruit = scanner.nextLine();
 
         System.out.println("nhap ngay san xuat trai cay vao");
-        String inputDateOfManufacture = scanner.nextLine();
+        String inputDateOfManufacture;
+        do {
+            System.out.println("nhap ngay san xuat trai cay vao");
+            inputDateOfManufacture = scanner.nextLine();
+        } while (!Regex.regexxDay(inputDateOfManufacture));
 
         System.out.println("nhap han su dung  trai cay vao");
         String inputExpiry = scanner.nextLine();
@@ -31,7 +44,7 @@ public class FruitSv implements IFruitSv{
         System.out.println("nhap gia san pham vao");
         String inputPrice = scanner.nextLine();
 
-        Fruit fruit = new Fruit(inputName, inputKindOfFruit,inputDateOfManufacture,inputExpiry,inputPrice);
+        Fruit fruit = new Fruit(inputName, inputKindOfFruit, inputDateOfManufacture, inputExpiry, inputPrice);
         fruitRepository.addRepo(fruit);
     }
 }
--- src/ss9_danh_sach/validate/Regex.java
+++ src/ss9_danh_sach/validate/Regex.java
@@ -1,5 +1,18 @@
 package ss9_danh_sach.validate;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Regex {
-    public static final String REGEXNAME = "";
+    public static final String REGEXNAME = "^[a-zA-Z ]+$";
+    public static final String REGEXDAY = "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$";
+    public static boolean regexName (String name){
+        return name.matches(REGEXNAME) ? true : false;
+    }
+    public static boolean regexxDay(String date) {
+        return date.matches(REGEXDAY) ? true : false;
+    }
+
+
 }
